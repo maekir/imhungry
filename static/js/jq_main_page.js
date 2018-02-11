eab177e4246bde71ed91c6fbe945a4fc250479c8
maekir <foyenas@gmail.com>
2018-02-11 23:45:21 +0300

    фильтр ингредиентов

---
+++ static/js/jq_main_page.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var search_ingredients = $("#ingredient_search");
+        ingredients_all = $("#ingredient_buttons *");
+        
+    ingredients_all.hide();
+
+    search_ingredients.on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+
+        ingredients_all.filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        }).show();
+
+        if (value == '') 
+            ingredients_all.hide();
+    });
+});
