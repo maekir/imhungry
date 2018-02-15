e01015e92a5cc7f8a9c23e00cdae15e45a8583c5
maekir <foyenas@gmail.com>
2018-02-15 22:05:27 +0300

    Revert "Merge branch 'design-djangod-pt2'"
    This reverts commit fa9d2ab75a2d7fe7f05998d2b56ceaf140d02951, reversing
    changes made to 4d406ff94a9ff3c8eac67059f5d254e828aa1d32.

--- ladybugs/forms.py
+++ ladybugs/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 
 
@@ -11,6 +11,7 @@ class SignUpForm(UserCreationForm):
     password2 = forms.CharField(label=("Подтвердите пароль"), widget=forms.PasswordInput, strip=False, help_text=(""))
     password1 = forms.CharField(label=("Пароль"), widget=forms.PasswordInput, strip=False, help_text=(
     "<br>Ваш пароль не должен быть похожим на ваши персональные данные, должен содержать минимум 8 символов и не может состоять только из чисел."))
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
@@ -24,12 +25,3 @@ class UserEditForm(forms.Form):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'information')
-
-
-class PasswordChangeForm(forms.Form):
-    old_password = forms.CharField(max_length=150, help_text='<br>', label='Старый пароль:', widget=forms.PasswordInput)
-    new_password1 = forms.CharField(max_length=150, label='Новый пароль:', help_text='<br>', widget=forms.PasswordInput)
-    new_password2 = forms.CharField(max_length=150, label='Подтвердите новый пароль:', help_text='<br>', widget=forms.PasswordInput)
-    class Meta:
-        model = User
-        fields = ('old_password', 'new_password1', 'new_password2')
